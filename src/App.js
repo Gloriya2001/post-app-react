eaf13d31730c1b01e3af00c12ee1474b2e9c9536
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-07 23:37:41 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPost from './components/AddPost';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
      <AddPost/>
+     <br></br>
+     <Search/>
     </div>
   );
 }
