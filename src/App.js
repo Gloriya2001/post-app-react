8250ba047921de66fb7369cb375e234631c8b906
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-08 00:37:29 +0530

    Nav added to all

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import AddPost from './components/AddPost';
 import Search from './components/Search';
 import Delete from './components/Delete';
 import ViewPost from './components/ViewPost';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+
+      <br></br>
      <AddPost/>
      <br></br>
      <Search/>
--- src/components/ViewPost.jsx
+++ src/components/ViewPost.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const ViewPost = () => {
     return (
         <div>
+
+<Navbar/>
 
             <div className="container">
                 <div className="row">
